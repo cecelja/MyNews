1129be818e8dcddd96520f2a518bf6a58d77f51b
Filip Cecelja <filip.cecelja@gmail.com>
2021-07-10 12:07:00 +0200

    Commit 1.04

--- app/src/main/java/com/example/mynews/MainActivity.java
+++ app/src/main/java/com/example/mynews/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.loader.app.LoaderManager;
 import androidx.loader.content.Loader;
 import androidx.viewpager.widget.ViewPager;
@@ -32,30 +33,23 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.main_activit);
         // Find the view pager that will allow the user to swipe between fragments
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
-        //Initiate a new fragmentadapter that maps which fragments are swiped and how
+        //Initiate a new fragment adapter that maps which fragments are swiped and how
         FragmentAdapter adapter = new FragmentAdapter(getSupportFragmentManager());
         //Set the adapter to the viewpager
         viewPager.setAdapter(adapter);
+
 
         BottomNavigationView bottom= (BottomNavigationView) findViewById(R.id.bottomNavigationView);
         bottom.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
-
-                if(item.getItemId() == R.id.News){
-                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                    startActivity(intent);
-                    return true;
-                } else if(item.getItemId() == R.id.Search){
-                    Intent searchIntent = new Intent(getApplicationContext(), SearchActivity.class);
-                    startActivity(searchIntent);
+                if(item.getItemId() == R.id.Search){
+                    openFragment(new SearchFragment());
                     return true;
                 } else if(item.getItemId() == R.id.Saved){
-                    Intent savedIntent = new Intent(getApplicationContext(), SavedActivity.class);
-                    startActivity(savedIntent);
+                    openFragment(new SavedFragment());
                     return true;
-                }
-
+                } else startActivity(new Intent(getApplicationContext(), MainActivity.class));
                 return false;
             }
         });
@@ -64,5 +58,11 @@ public class MainActivity extends AppCompatActivity{
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
     }
 
+    void openFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.container, fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
 
 }
---
+++ app/src/main/java/com/example/mynews/SavedFragment.java
@@ -0,0 +1,30 @@
+package com.example.mynews;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import org.jetbrains.annotations.NotNull;
+
+public class SavedFragment extends Fragment {
+    @Nullable
+    @org.jetbrains.annotations.Nullable
+    @Override
+    public View onCreateView(@NonNull @NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        if(container != null){
+            container.removeAllViews();
+        }
+        View rootView = inflater.inflate(R.layout.saved_activity, container, false);
+        return rootView;
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+}
